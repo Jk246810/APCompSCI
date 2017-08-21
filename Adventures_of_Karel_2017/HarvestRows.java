58e3226d90e3a4b0f8a8e3e1a0c2a900ede8240b
Jamee Krzanich <jamee@Jamees-MacBook-Pro.local>
2017-08-21 10:57:18 -0700

    karel the robot

---
+++ Adventures_of_Karel_2017/HarvestRows.java
@@ -0,0 +1,26 @@
+
+import stanford.karel.*;
+
+public class HarvestRows extends Karel
+{
+
+    public void run()
+    {
+
+    }
+
+    /* IGNORE THE CODE BELOW.  (Don't delete it, but we
+     * had to include it to get Karel to play nicely with
+     * BlueJ.  You don't need to understand what it means
+     * at all.  */
+    public static void launchKarelEnvironment()
+    {
+        String[] args = new String[1];
+        String name = new SecurityManager() {
+            public String className() 
+                {return this.getClassContext()[1].getCanonicalName();}
+            }.className();
+        args[0] = "code=" + name;         
+        Karel.main(args);
+    }
+}
