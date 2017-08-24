e1e92f11a10fa41ede304c1a915e9fdbbf553687
Jamee Krzanich <jamee@Jamees-MacBook-Pro.local>
2017-08-23 21:41:19 -0700

    Spring Time

--- Adventures_of_Karel_2017/SpringTime.java
+++ Adventures_of_Karel_2017/SpringTime.java
@@ -1,13 +1,89 @@
 
 import stanford.karel.*;
 
+
+//Jamee Krzanich, Period C
 public class SpringTime extends Karel
 {
 
     public void run()
     {
-
+  
+        for (int i=0; i<5; i++)
+       {
+           buildFlower();
+       }
+        
     }
+    public void buildFlower()
+    {
+        moveToStem();
+        ascendStem();
+        constructFlower();
+        descendStem();
+    }
+    
+    public void moveToStem()
+    {
+       while (frontIsClear()) 
+       {
+           move();
+           
+       }
+       
+    }
+    
+    public void ascendStem() 
+    {
+        turnLeft();
+        while (rightIsBlocked())
+        {
+            move();
+        }
+        
+    }
+    
+    public void constructFlower()
+    {
+        
+        if (rightIsClear())
+        {
+            putBeeper();
+            for (int i=0; i<3; i++)
+            {
+               
+               move();
+               putBeeper();
+               turnRight(); 
+
+            }
+            
+        }
+        
+    }
+    
+    public void descendStem()
+    {
+        
+        turnLeft();
+        
+        while (frontIsClear())
+        {
+            move();
+        }
+        turnLeft();
+        
+    }
+   
+    
+    public void turnRight()
+    {
+        for (int i=0; i<3; i++)
+        {
+            turnLeft();
+        }
+    }
+    
 
     /* IGNORE THE CODE BELOW.  (Don't delete it, but we
      * had to include it to get Karel to play nicely with
