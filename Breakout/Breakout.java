6e5f5071e09cb6445fd3bfa2be8f78e99594d00c
Jamee Krzanich <jamee@Jamees-MacBook-Pro.local>
2017-09-13 14:10:50 -0700

    finished collision with paddle

--- Breakout/Breakout.java
+++ Breakout/Breakout.java
@@ -52,13 +52,154 @@ public class Breakout extends GraphicsProgram
  /** Global variables declared here.  You should feel free to add others as needed. */
      GRect paddle;
      GOval ball;
+     GOval movingBall;
      double dx;
      double dy;
     
 /** Runs the Breakout program. */
     public void run() 
     {
+        initBricks();
+        initPaddle();
+        initBall();
+        addMouseListeners();
+        while(true)
+        {
+            pause(10);
+            updateBall();
+            checkForCollision(); 
+        }
+        
 
     }
+    
+    GRect blocks;
+    GRect movingPaddle;
+    
+    public void initBricks()
+    {
+        for(int r=0; r<NBRICK_ROWS; r++)
+        {
+            for (int c=0; c<NBRICK_ROWS; c++)
+            {
+                blocks = new GRect(0 + c*(BRICK_WIDTH + BRICK_SEP), 
+                                        BRICK_Y_OFFSET + r*(BRICK_HEIGHT + BRICK_SEP),
+                                        BRICK_WIDTH,
+                                        BRICK_HEIGHT);
+                
+                if(r<2)
+                {
+                    blocks.setColor(Color.RED);  
+                }else if(r<4)
+                {
+                    blocks.setColor(Color.ORANGE);
+                }else if(r<6)
+                {
+                    blocks.setColor(Color.YELLOW);
+                }else if(r<8)
+                {
+                    blocks.setColor(Color.GREEN);
+                }else 
+                {
+                    blocks.setColor(Color.CYAN);
+                }
+                
+                blocks.setFilled(true);
+                add(blocks);
+            }
+           
+        }
+    }
+    
+    public void initPaddle()
+    {
+        movingPaddle = new GRect(0,
+                           HEIGHT - PADDLE_Y_OFFSET,
+                           PADDLE_WIDTH, 
+                           PADDLE_HEIGHT);
+        
+       movingPaddle.setFilled(true);
+       add(movingPaddle);
+        
+    }
+    
+    public void initBall()
+    {
+        
+        movingBall = new GOval(WIDTH/2, HEIGHT/2, BALL_RADIUS*2, BALL_RADIUS*2);
+        dx = 1+ Math.random()*2;
+        if(Math.random()>0.5)
+        {
+            dx = -dx;
+           
+
+        }
+        dy = 3;
+        
+        movingBall.setFilled(true);
+        add(movingBall);
+        
+    }
+    
+    public void updateBall()
+    {
+       movingBall.move(dx, dy);
+       
+       
+        
+          if (movingBall.getX()<0)
+          {
+            dx = -dy;   
+          }         
+          if (movingBall.getX()>WIDTH)
+          {
+            dx = -dx;   
+          }
+          if (movingBall.getY()<0)
+          {
+            dy = -dy;   
+          }         
+          if (movingBall.getY()>HEIGHT)
+          {
+            dy = -dy;   
+          }
+    }
+    
+    public void checkForCollision()
+    {
+        GObject object = getElementAt(movingBall.getX(), movingBall.getY());
+        
+        if (object == movingPaddle)
+        {
+            dy = -dy;
+        }else if (object == blocks)
+        {
+            remove(object); 
+        }else{
+            
+        }
+        
+    }
+    
+    public void mouseMoved(MouseEvent event)
+    {
+        int x = event.getX();
+        if (x < 0 + PADDLE_WIDTH/2)
+        {
+            movingPaddle.setLocation(0 ,HEIGHT - PADDLE_Y_OFFSET);
+            
+        }else if (x > WIDTH - PADDLE_WIDTH/2)
+        {
+            movingPaddle.setLocation(WIDTH - PADDLE_WIDTH ,HEIGHT - PADDLE_Y_OFFSET);
+        }
+        else{
+            
+            movingPaddle.setLocation(x - PADDLE_WIDTH/2,HEIGHT - PADDLE_Y_OFFSET); 
+        }
+    }
+    
+   
+    
+    
    
 }
