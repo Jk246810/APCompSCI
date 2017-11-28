55c5bec757848688e7c37c8123bbaa770f019ab4
Jk246810 <jameekr22@gmail.com>
2017-11-27 17:56:10 -0800

    picture lab added

---
+++ PictureLab2017/Picture.java
@@ -0,0 +1,177 @@
+import java.awt.*;
+import java.awt.font.*;
+import java.awt.geom.*;
+import java.awt.image.BufferedImage;
+import java.text.*;
+import java.util.*;
+import java.util.List; // resolves problem with java.awt.List and java.util.List
+
+/**
+ * A class that represents a picture.  This class inherits from 
+ * SimplePicture and allows the student to add functionality to
+ * the Picture class.  
+ * 
+ * @author Barbara Ericson ericson@cc.gatech.edu
+ */
+public class Picture extends SimplePicture 
+{
+    ///////////////////// constructors //////////////////////////////////
+
+    /**
+     * Constructor that takes no arguments 
+     */
+    public Picture ()
+    {
+        /* not needed but use it to show students the implicit call to super()
+         * child constructors always call a parent constructor 
+         */
+        super();  
+    }
+
+    /**
+     * Constructor that takes a file name and creates the picture 
+     * @param fileName the name of the file to create the picture from
+     */
+    public Picture(String fileName)
+    {
+        // let the parent class handle this fileName
+        super(fileName);
+    }
+
+    /**
+     * Constructor that takes the width and height
+     * @param height the height of the desired picture
+     * @param width the width of the desired picture
+     */
+    public Picture(int height, int width)
+    {
+        // let the parent class handle this width and height
+        super(width,height);
+    }
+
+    /**
+     * Constructor that takes a picture and creates a 
+     * copy of that picture
+     * @param copyPicture the picture to copy
+     */
+    public Picture(Picture copyPicture)
+    {
+        // let the parent class do the copy
+        super(copyPicture);
+    }
+
+    /**
+     * Constructor that takes a buffered image
+     * @param image the buffered image to use
+     */
+    public Picture(BufferedImage image)
+    {
+        super(image);
+    }
+
+    ////////////////////// methods ///////////////////////////////////////
+
+    /**
+     * Method to return a string with information about this picture.
+     * @return a string with information about the picture such as fileName,
+     * height and width.
+     */
+    public String toString()
+    {
+        String output = "Picture, filename " + getFileName() + 
+            " height " + getHeight() 
+            + " width " + getWidth();
+        return output;
+
+    }
+
+    public void clearBlue()
+    {
+        Pixel[] pixels = getPixels(); 
+        for(Pixel pixel : pixels) 
+        {
+            pixel.setBlue(0);
+        }
+    }
+    public void allRed()
+    {
+        Pixel[] pixels = getPixels(); 
+        for(Pixel pixel : pixels) 
+        {
+            pixel.setRed(255);
+        }
+    }
+    public void swapGreenBlue()
+    {
+        
+        Pixel[] pixels = getPixels(); 
+        for(Pixel pixel : pixels) 
+        {
+            int origGreen = pixel.getGreen();
+            pixel.setGreen(pixel.getBlue());
+            pixel.setBlue(origGreen); 
+        }
+    }
+    public void negative()
+    {
+        
+        Pixel[] pixels = getPixels(); 
+        for(Pixel pixel : pixels) 
+        {
+            pixel.setRed(255 - pixel.getRed()); 
+            pixel.setGreen(255 - pixel.getGreen()); 
+            pixel.setBlue(255 - pixel.getBlue()); 
+            
+        }
+    }
+    public void greyScale()
+    {
+        
+        Pixel[] pixels = getPixels(); 
+        for(Pixel pixel : pixels) 
+        {
+            int avg = (pixel.getRed() + pixel.getGreen() + pixel.getGreen())/3;
+            pixel.setRed(avg); 
+            pixel.setGreen(avg); 
+            pixel.setBlue(avg); 
+            
+        }
+    }
+    
+    public void solveGold()
+    {
+        
+        Pixel[] pixels = getPixels(); 
+        for(Pixel pixel : pixels) 
+        {
+            pixel.setRed(pixel.getRed()*10); 
+            pixel.setGreen(0); 
+            pixel.setBlue(0); 
+            
+        }
+    }
+    public void solveCopper()
+    {
+        
+        Pixel[] pixels = getPixels(); 
+        for(Pixel pixel : pixels) 
+        {
+            pixel.setRed(0); 
+            pixel.setGreen(pixel.getGreen()*10); 
+            pixel.setBlue(pixel.getBlue()*10); 
+            
+        }
+    }
+
+    /* Main method for testing - each class in Java can have a main 
+     * method 
+     */
+    public static void main(String[] args) 
+    {
+        Picture beach = new Picture("images/" + "beach.jpg");
+        beach.explore();
+        //beach.clearBlue();
+        //beach.explore();
+    }
+
+} // this } is the end of class Picture, put all new methods before this
